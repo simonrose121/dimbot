62513f01bed8345cfcf81920ad56bdca21b041c1
Simon Rose <simonrose121@gmail.com>
2015-10-01 15:26:29 +0100

    Initial stylesheet setup with grunt watch

---
+++ gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ["client/assets/css"]
+        },
+          files: {"client/assets/css/styles.css": "client/assets/css/styles.less"}
+        }
+      },
+      watch: {
+        files: "client/assets/css/*",
+        tasks: ["less"]
+      }
+   });
+   grunt.loadNpmTasks('grunt-contrib-less');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.registerTask('default', ['less']);
+ };
