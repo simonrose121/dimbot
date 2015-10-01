50091d9f7fc220ad864b5459167450c6b6031528
Simon Rose <simonrose121@gmail.com>
2015-10-01 12:30:40 +0100

    Using main index to handle separate pages and behaviours

--- app.js
+++ app.js
@@ -16,16 +16,13 @@ app.use(bodyParser.json());
 
 // page routes
 app.get('/', function(req,res) {
-    res.render(__dirname + '/client/components/game/gameView.jade');
+    res.render(__dirname + '/client/index.jade');
 });
 
 // static files
-app.use('/js', express.static(__dirname + '/client/assets/js'));
 app.use('/libs', express.static(__dirname + '/client/assets/libs'));
 app.use('/css', express.static(__dirname + '/client/assets/css'));
 app.use('/img', express.static(__dirname + '/client/assets/img'));
-app.use('/font', express.static(__dirname + '/client/assets/font'));
-app.use('/views', express.static(__dirname + '/client/assets/views'));
 
 app.listen(port);
 
---
+++ client/app.module.js
@@ -0,0 +1,1 @@
+var dimbot = angular.module('dimbotApp', ['ngResource']);
