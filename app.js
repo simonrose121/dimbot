d6a28322da06dea6bad9ca5ba44be6b9b3f3eaeb
Simon Rose <simonrose121@gmail.com>
2015-10-01 12:21:21 +0100

    Reorganisation of client application

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ app.use(bodyParser.json());
 
 // page routes
 app.get('/', function(req,res) {
-    res.render(__dirname + '/client/views/index.jade');
+    res.render(__dirname + '/client/components/game/gameView.jade');
 });
 
 // static files
-app.use('/js', express.static(__dirname + '/client/js'));
-app.use('/libs', express.static(__dirname + '/client/libs'));
-app.use('/css', express.static(__dirname + '/client/css'));
-app.use('/img', express.static(__dirname + '/client/img'));
-app.use('/font', express.static(__dirname + '/client/font'));
-app.use('/views', express.static(__dirname + '/client/views'));
+app.use('/js', express.static(__dirname + '/client/assets/js'));
+app.use('/libs', express.static(__dirname + '/client/assets/libs'));
+app.use('/css', express.static(__dirname + '/client/assets/css'));
+app.use('/img', express.static(__dirname + '/client/assets/img'));
+app.use('/font', express.static(__dirname + '/client/assets/font'));
+app.use('/views', express.static(__dirname + '/client/assets/views'));
 
 app.listen(port);
 
