6cf9402b3ec7d2933b27ac5d320468ce3bddfec8
Simon Rose <simonrose121@gmail.com>
2015-10-03 16:24:48 +0100

    Instruction list in game controller

--- client/components/game/game-controller.js
+++ client/components/game/game-controller.js
@@ -1,3 +1,6 @@
 app.controller('dimbot.game', ['$scope', '$http', function($scope, $http) {
+	$scope.instructionList = [];
 
+	$scope.instructionList.push("fwd");
+	$scope.instructionList.push("light");
 }]);
--- client/components/game/game-controller_tests.js
+++ client/components/game/game-controller_tests.js
@@ -1,7 +1,24 @@
 describe('Game Controller', function() {
-	beforeEach(module('dimbot.game'));
+	beforeEach(module('dimbot'));
 
-	it('test method', function() {
-		expect(true).toBe(true);
-	})
+	// Get app controller
+	var $controller;
+
+	beforeEach(inject(function(_$controller_){
+	    // The injector unwraps the underscores (_) from around the parameter names when matching
+	    $controller = _$controller_;
+	}));
+
+	// Get test specific controller
+	var $scope, controller;
+
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('dimbot.game', { $scope: $scope });
+    });
+
+	it('Instruction list is initialised on app start', function() {
+		expect($scope.instructionList).toBeDefined();
+		expect($scope.instructionList[0]).toEqual('fwd');
+	});
 });
