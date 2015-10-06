c46849eea127e1de6b0ecdc4fc9c690144e4a99c
Simon Rose <simonrose121@gmail.com>
2015-10-06 12:23:49 +0100

    Adding instructions to program on click

--- client/components/game/game.js
+++ client/components/game/game.js
@@ -13,8 +13,16 @@
 		var up = new Instruction("up", "client/assets/img/up-instruction.png");
 		var fwd = new Instruction("fwd", "client/assets/img/up-instruction.png");
 
+		vm.update = function() {
+			vm.program = ProgramService.getProgram();
+		}
+
+		vm.addToProgram = function(ins) {
+			ProgramService.addInstruction(ins);
+			vm.update();
+		}
+
 		// Setup page
-		vm.program = ProgramService.getProgram();
 		vm.instructions = [up, fwd];
 	};
 })();
