60df68dc7218ca0410a97c7819bf7bf6df239714
Simon Rose <simonrose121@gmail.com>
2015-10-06 17:16:12 +0100

    Adding directive to handle grid

---
+++ client/components/game/directives/grid.directive.js
@@ -0,0 +1,13 @@
+(function() {
+	angular
+		.module('dimbot.game')
+		//.directive('GridDirective', GridDirective)
+
+	function GridDirective() {
+		var directive = {
+
+		};
+
+		return directive;
+	};
+})();
