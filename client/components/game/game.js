9600e3fc1a2f7cf7e3fa22ede7ec486c22f0d0b4
Simon Rose <simonrose121@gmail.com>
2015-10-07 09:17:02 +0100

    Updated game controller to utilise function hoisting and increase readability

--- client/components/game/game.js
+++ client/components/game/game.js
@@ -8,28 +8,35 @@
 	function Game($http, programService, Instruction) {
 		var vm = this;
 
+		vm.addToProgram = addToProgram;
+		vm.instructions = [];
+		vm.refresh = refresh;
+		vm.removeFromProgram = removeFromProgram;
+
 		// setup some default instructions using the service
-		// TODO: move this to a method
+		// TODO: move this to out to a service to be populated in as a level
 		var up = new Instruction("up", "client/assets/img/up-instruction.png");
-		var right = new Instruction("right", "client/assets/img/right-instruction.png");
+		var right = new Instruction("right",
+			"client/assets/img/right-instruction.png");
 
-		// ensure that DOM always matches program in program service
-		vm.update = function() {
-			vm.program = programService.getProgram();
-		};
-
-		vm.addToProgram = function(ins) {
+		function addToProgram(ins) {
 			programService.addInstruction(ins);
 			vm.update();
 		};
 
-		vm.removeFromProgram = function(index) {
+		vm.instructions = [up, right];
+
+		function removeFromProgram(index) {
 			programService.removeInstruction(index);
 			vm.update();
 		};
 
-		// Setup page
-		vm.update();
-		vm.instructions = [up, right];
+		// ensure that DOM always matches program in program service
+		function refresh() {
+			vm.program = programService.getProgram();
+		};
+
+		// call update to add default instructions
+		vm.refresh();
 	};
 })();
