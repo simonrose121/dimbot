03af19bc63edb74b35fa8aa0c88e2dd1951e3f23
Simon Rose <simonrose121@gmail.com>
2015-10-19 12:28:08 +0100

    Testing instruction factory for all types of instructions

--- client/components/game/factories/instruction.factory.spec.js
+++ client/components/game/factories/instruction.factory.spec.js
@@ -11,7 +11,7 @@ describe('Instruction Factory', function() {
 		expect(factory).toBeDefined();
 	});
 
-	it('Can instantiate instructions using factory', function() {
+	it('Can instantiate foward instruction using factory', function() {
 		// arrange
 		var newInstruction;
 
@@ -22,5 +22,44 @@ describe('Instruction Factory', function() {
 		expect(newInstruction).toBeDefined();
 		expect(newInstruction.name).toEqual('fw');
 		expect(newInstruction.src).toEqual('client/assets/img/up-instruction.png');
+	});
+
+	it('Can instantiate right rotate instruction using factory', function() {
+		// arrange
+		var newInstruction;
+
+		// act
+		newInstruction = factory.getInstruction('rr');
+
+		// assert
+		expect(newInstruction).toBeDefined();
+		expect(newInstruction.name).toEqual('rr');
+		expect(newInstruction.src).toEqual('client/assets/img/right-rotate-instruction.png');
+	});
+
+	it('Can instantiate left rotate instruction using factory', function() {
+		// arrange
+		var newInstruction;
+
+		// act
+		newInstruction = factory.getInstruction('rl');
+
+		// assert
+		expect(newInstruction).toBeDefined();
+		expect(newInstruction.name).toEqual('rl');
+		expect(newInstruction.src).toEqual('client/assets/img/left-rotate-instruction.png');
+	});
+
+	it('Can instantiate light instruction using factory', function() {
+		// arrange
+		var newInstruction;
+
+		// act
+		newInstruction = factory.getInstruction('lt');
+
+		// assert
+		expect(newInstruction).toBeDefined();
+		expect(newInstruction.name).toEqual('lt');
+		expect(newInstruction.src).toEqual('client/assets/img/lightbulb.png');
 	});
 });
