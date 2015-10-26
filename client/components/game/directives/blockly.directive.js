3e78117e0a73353821b970e5faf2b8b710f636b7
Simon Rose <simonrose121@gmail.com>
2015-10-26 10:59:46 +0000

    Changed positioning of blockly div

--- client/components/game/directives/blockly.directive.js
+++ client/components/game/directives/blockly.directive.js
@@ -8,15 +8,38 @@
 	function dimBlocklyDirective(logger) {
 		var directive = {
 			restrict: 'E',
-			link: link,
-			//templateUrl: "<div id='blocklyDiv'></div>"
+			link: link
 		};
 
 		return directive;
 
 		function link(scope, elem) {
-			var workspace = Blockly.inject('blocklyDiv',
-				{toolbox: document.getElementById('toolbox')});
+			var vm = this;
+
+			vm.customBlocks = customBlocks;
+			vm.init = init;
+
+			vm.customBlocks();
+			vm.init();
+
+			function customBlocks() {
+				Blockly.Blocks['move_forward'] = {
+				  init: function() {
+				    this.appendValueInput("1")
+				        .setCheck("Number")
+				        .setAlign(Blockly.ALIGN_RIGHT)
+				        .appendField("Forward");
+				    this.setColour(260);
+				    this.setTooltip('');
+				    this.setHelpUrl('http://www.example.com/');
+				  }
+				};
+			}
+
+			function init() {
+				var workspace = Blockly.inject('instructions-inner',
+					{toolbox: document.getElementById('toolbox')});
+			}
 		}
 	}
 })();
