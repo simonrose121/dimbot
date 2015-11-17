a874f62316c6df4b49d74a91ddceb6b65e6b4b22
Simon Rose <simonrose121@gmail.com>
2015-11-17 10:58:56 +0000

    Mocking http request to test log service

--- client/components/game/services/log/log.service.spec.js
+++ client/components/game/services/log/log.service.spec.js
@@ -9,18 +9,28 @@ describe('Log Service', function() {
 		$httpBackend = $injector.get('$httpBackend');
 	}));
 
+	var mockResponse = {
+		"user_id" : 5,
+		"type" : "button_press",
+		"summary" : "button rewind",
+		"message" : "Pressed rewind",
+		"timestamp" : "2015-11-17T10:45:18.511Z",
+		"__v" : 0
+	}
+
 	it('Can post log and recieve response', function() {
 		// arrange
 		var instruction = instructionFactory.getInstruction('fw');
 
 		// act
+		$httpBackend
+			.whenPOST('/log/')
+			.respond(200, mockResponse);
+
 		service.addedInstruction(instruction, 'click', 0);
 
-		$httpBackend
-			.when('POST', 'localhost:8079/log/')
-			.respond(200);
-
+		$httpBackend.flush();
 		// assert
-		//expect(ins).toBeDefined();
+		expect(instruction).toBeDefined();
 	});
 });
